031d35f5d84f9270a895ba9565870e9bce2208a6
Miguel <migueesquivelrios@gmail.com>
2026-06-09 15:31:16 -0300

    clase 25

--- icard_django/icard/icard/urls.py
+++ icard_django/icard/icard/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import include, path, re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+from users.api.router import router_user
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -19,4 +21,5 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include(router_user.urls)),
 ]
